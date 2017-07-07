f1c214a3469dfd8faf1c52b32ba752db112ec48c
Davykins <dgershnik@gmail.com>
2017-07-06 19:13:43 -0700

    GUYS HELP ME I'M POOPING A CRAP TON AND I'M OUT OF TP THIS IS THE END

--- confession.py
+++ confession.py
@@ -1,1 +1,11 @@
-print "poop"
+input = f.open("confession.txt")
+lines = input.readlines()
+print lines
+for i in range(1, int(lines[0])+1):
+    currentprob = lines[i].strip().split()
+    wins - {}
+    loses = {}
+    if currentprob[0] not in wins.keys():
+        wins[currentprob[0]]=1
+        if currentprob[1] not in loses.keys():
+            loses
